f5cae29943bd6155e8de3b460334756b9011affa
JongHun Kim(boongDev) <qnddjKJH.dev@gmail.com>
2022-07-03 16:40:35 +0900

    update PointHistoryService
     - 전체 내역 조회
     - 사용자 내역 조회
     - 사용자 총점 조회
     - 포인트 발급 이벤트
      - 생성
      - 수정
      - 삭제
    
     - Todo
      - 장소별 내역 조회
      - 리뷰별 내역 조회
      - 내역 조회 필터 추가

--- src/main/java/com/triple/point/domain/points/repository/PointsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.triple.point.domain.points.repository;
-
-import com.triple.point.domain.points.entity.Points;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PointsRepository extends JpaRepository<Points, UUID> {
-}
