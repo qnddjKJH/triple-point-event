8f0f8ed0272a8aaf80d73bfe280282e0df8fcfea
JongHun Kim(boongDev) <qnddjKJH.dev@gmail.com>
2022-07-03 16:47:41 +0900

    update PointsController
     - /points
      - GET : "" > 전체내역 조회
      - GET : /{userId} > 사용자 내역 조회
      - GET : /{userId}/total-point > 사용자 총점 조회
      - POST : "" > 포인트 이벤트 생성
    
     - Todo
      - 장소별 내역 조회
      - 리뷰별 내역 조회

--- src/main/java/com/triple/point/domain/points/controller/PointsController.java
+++ src/main/java/com/triple/point/domain/points/controller/PointsController.java
@@ -1,4 +1,70 @@
 package com.triple.point.domain.points.controller;
 
+import com.triple.point.domain.points.dto.PointHistoryRequest;
+import com.triple.point.domain.points.dto.PointHistoryResponse;
+import com.triple.point.domain.points.dto.TotalPointResponse;
+import com.triple.point.domain.points.service.PointHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequestMapping("/points")
+@RequiredArgsConstructor
+@RestController
 public class PointsController {
+
+    private final PointHistoryService pointHistoryService;
+
+    @GetMapping("")
+    public ResponseEntity<List<PointHistoryResponse>> allHistory() {
+        List<PointHistoryResponse> allPointHistories = pointHistoryService.getAllPointHistories();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(allPointHistories);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<PointHistoryResponse>> getUserHistories(@PathVariable("userId") String userId) {
+        List<PointHistoryResponse> userPointHistories = pointHistoryService.getUserPointHistories(userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(userPointHistories);
+    }
+
+    /**
+     * 사용자 포인트 총점 조회 API
+     *
+     * */
+    @GetMapping("/{userId}/total-point")
+    public ResponseEntity<TotalPointResponse> userTotalPoint(@PathVariable("userId") String userId) {
+        int userTotalPoint = pointHistoryService.userTotalPoint(userId);
+        TotalPointResponse response = TotalPointResponse.builder()
+                .userId(userId)
+                .totalPoint(userTotalPoint)
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<PointHistoryResponse> savePointHistory(@RequestBody PointHistoryRequest request) {
+        log.info("[POST] :: /posts, request :: {}", request);
+        PointHistoryResponse pointHistoryResponse = pointHistoryService.pointsServiceManager(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(pointHistoryResponse);
+    }
 }
