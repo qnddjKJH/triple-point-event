f79d576dd95a0155a2d5d3939bb0e68773cf12b4
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-07-05 17:07:34 +0900

    refactor EventServiceProxy

--- src/main/java/com/triple/point/proxy/EventServiceProxy.java
+++ src/main/java/com/triple/point/proxy/EventServiceProxy.java
@@ -2,25 +2,22 @@ package com.triple.point.proxy;
 
 import com.triple.point.domain.common.dto.EventRequest;
 import com.triple.point.domain.common.dto.EventResponse;
-import com.triple.point.domain.points.entity.ActionType;
-import com.triple.point.domain.points.entity.EventType;
+import com.triple.point.domain.common.type.ActionType;
+import com.triple.point.domain.common.type.EventType;
 import com.triple.point.domain.points.service.EventService;
 import com.triple.point.domain.points.service.PointHistoryService;
-import com.triple.point.domain.points.service.TestUserEventService;
-import lombok.RequiredArgsConstructor;
+import com.triple.point.domain.points.service.ExampleUserEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Service
 public class EventServiceProxy  {
-    private Map<String, EventService> map = new HashMap<>();
+    private Map<String, EventService> serviceMap = new HashMap<>();
     private final Map<String, String> methodMap = Map.of(
             "ADD", "addEvent",
             "MOD", "modifyEvent",
@@ -29,16 +26,15 @@ public class EventServiceProxy  {
 
     public EventServiceProxy(
             @Autowired PointHistoryService pointHistoryService,
-            @Autowired TestUserEventService testUserEventService) {
-        map.put("REVIEW", pointHistoryService);
-        map.put("USER", testUserEventService);
+            @Autowired ExampleUserEventService exampleUserEventService) {
+        serviceMap.put("REVIEW", pointHistoryService);
+        serviceMap.put("USER", exampleUserEventService);
     }
 
     public EventResponse invoke(EventType type, ActionType action, EventRequest request) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Class<? extends EventService> eventServiceClass = map.get(type.name()).getClass();
+        Class<? extends EventService> eventServiceClass = serviceMap.get(type.name()).getClass();
         Method method = eventServiceClass.getMethod(methodMap.get(action.name()), EventRequest.class);
 
-        return (EventResponse) method.invoke(map.get(type.name()), request);
+        return (EventResponse) method.invoke(serviceMap.get(type.name()), request);
     }
-
 }
