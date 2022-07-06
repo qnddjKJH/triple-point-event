abc5bd201251bbe8bb001c9a19a21eaaf96d24fd
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-07-06 15:58:01 +0900

    update EventServiceProxy
     - add Bad Request Exception

--- src/main/java/com/triple/point/proxy/EventServiceProxy.java
+++ src/main/java/com/triple/point/proxy/EventServiceProxy.java
@@ -2,8 +2,10 @@ package com.triple.point.proxy;
 
 import com.triple.point.domain.common.dto.EventRequest;
 import com.triple.point.domain.common.dto.EventResponse;
+import com.triple.point.domain.common.exception.CustomException;
 import com.triple.point.domain.common.type.ActionType;
 import com.triple.point.domain.common.type.EventType;
+import com.triple.point.domain.common.type.ExceptionType;
 import com.triple.point.domain.events.service.EventsService;
 import com.triple.point.domain.events.service.EventsReviewPointService;
 import com.triple.point.domain.events.service.ExampleUserEventsService;
@@ -32,9 +34,27 @@ public class EventServiceProxy  {
     }
 
     public EventResponse invoke(EventType type, ActionType action, EventRequest request) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        isConfirmReq(type, action);
         Class<? extends EventsService> eventServiceClass = serviceMap.get(type.name()).getClass();
         Method method = eventServiceClass.getMethod(methodMap.get(action.name()), EventRequest.class);
 
         return (EventResponse) method.invoke(serviceMap.get(type.name()), request);
+    }
+
+    private void isConfirmReq(EventType type, ActionType action) {
+        isCorrectType(type);
+        isCorrectAction(action);
+    }
+
+    private void isCorrectType(EventType type) {
+        if (!serviceMap.containsKey(type.name())) {
+            throw new CustomException(ExceptionType.BAD_REQUEST_TYPE, type);
+        }
+    }
+
+    private void isCorrectAction(ActionType action) {
+        if (!serviceMap.containsKey(action.name())) {
+            throw new CustomException(ExceptionType.BAD_REQUEST_ACTION, action);
+        }
     }
 }
